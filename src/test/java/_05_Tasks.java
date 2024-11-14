70c62a3f5b61593a3166aa4ddd3d2c6c8b7f52c3
TechnoStudy <ismettechno@gmail.com>
2024-11-14 17:34:59 +0000

    Task 1

---
+++ src/test/java/_05_Tasks.java
@@ -0,0 +1,47 @@
+
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class _05_Tasks {
+    /**
+     * Task 1
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * expect content type JSON
+     * expect title in response body to be "quis ut nam facilis et officia qui"
+     */
+
+    @Test
+    public void Task1()
+    {
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("title", equalTo("quis ut nam facilis et officia qui"))
+        ;
+    }
+
+    /**
+     * Task 2
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * expect content type JSON
+     *a) expect response completed status to be false(hamcrest)
+     *b) extract completed field and testNG assertion(testNG)
+     */
+
+
+
+
+
+
+}
